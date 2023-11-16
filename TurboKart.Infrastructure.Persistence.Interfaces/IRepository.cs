65027ce7c3beb76305b7c8d21c2e47d03ed0b053
Jonas-Beck <jona63m2@edu.campusvejle.dk>
2023-11-16 13:31:26 +0100

    Added IRepository<T>

---
+++ TurboKart.Infrastructure.Persistence.Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace TurboKart.Infrastructure.Persistence.Interfaces
+{
+    public interface IRepository<T>
+    {
+        void Save(T entity);
+        IEnumerable<T> GetAll();
+
+        T GetBy(object id);
+
+
+
+
+
+
+
+
+    }
+}
