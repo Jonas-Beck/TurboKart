b0a7a694f282fecbd0610f579ce60c2dcc4ba270
Jonas-Beck <jona63m2@edu.campusvejle.dk>
2023-11-20 11:41:42 +0100

    Added Use case for booking

---
+++ TurboKart.Application.UseCases/BookingUseCase.cs
@@ -0,0 +1,32 @@
+﻿
+using TurboKart.Application.Interfaces;
+using TurboKart.Domain.Entities;
+using TurboKart.Infrastructure.Persistence.Interfaces;
+
+namespace TurboKart.Application.UseCases
+{
+    public class BookingUseCase : IBookingUseCase
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public BookingUseCase(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public void BookNew(DateTime dateTime, Customer customer)
+        {
+            Booking booking = new Booking() { Start = dateTime };
+        }
+
+        public IEnumerable<Booking> GetAllBookings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Booking> GetTodaysBookings()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
