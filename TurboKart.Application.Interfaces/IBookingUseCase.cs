d3533ee6b9ede797c88a25ec5b7dc61b92492629
Jonas-Beck <jona63m2@edu.campusvejle.dk>
2023-11-20 11:26:01 +0100

    Added Application interface

---
+++ TurboKart.Application.Interfaces/IBookingUseCase.cs
@@ -0,0 +1,11 @@
+﻿using TurboKart.Domain.Entities;
+
+namespace TurboKart.Application.Interfaces
+{
+    public interface IBookingUseCase
+    {
+        void BookNew(DateTime dateTime, Customer customer);
+        IEnumerable<Booking> GetAllBookings();
+        IEnumerable<Booking> GetTodaysBookings();
+    }
+}
