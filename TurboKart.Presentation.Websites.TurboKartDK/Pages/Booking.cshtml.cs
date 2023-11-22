36af032894a106ec4afd6771eae29b0b71094457
Jonas-Beck <jona63m2@edu.campusvejle.dk>
2023-11-22 11:50:32 +0100

    Added TurboKartDK Booking page with ModelBinding

--- TurboKart.Presentation.Websites.TurboKartDK/Pages/Booking.cshtml.cs
+++ TurboKart.Presentation.Websites.TurboKartDK/Pages/Booking.cshtml.cs
@@ -1,11 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace TurboKart.Presentation.Websites.TurboKartDK
 {
     public class BookingModel : PageModel
     {
+        [BindProperty]
+        public string Name { get; set; }
+
+        [BindProperty]
+        public string Email { get; set; }
+
+        [BindProperty]
+        public double PhoneNumber { get; set; }
+
+        [BindProperty]
+        public string GrandprixType { get; set; }
+
+        [BindProperty]
+        public string DriverCount { get; set; }
+
+        [BindProperty]
+        public DateOnly Date { get; set; }
+
+        [BindProperty]
+        public TimeOnly Time { get; set; }
+
+
+
         public void OnGet()
         {
+
         }
+
+        public void OnPost()
+        {
+
+        }
+
     }
 }
