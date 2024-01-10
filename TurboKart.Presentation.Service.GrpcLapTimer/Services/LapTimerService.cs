838f85e7be721066916ecca22dd79b0dd76fb897
Jonas-Beck <jona63m2@edu.campusvejle.dk>
2024-01-10 13:42:55 +0100

    Override CartCrossed gRPC Method

--- TurboKart.Presentation.Service.GrpcLapTimer/Services/LapTimerService.cs
+++ TurboKart.Presentation.Service.GrpcLapTimer/Services/LapTimerService.cs
@@ -1,3 +1,4 @@
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 
 namespace TurboKart.Presentation.Service.GrpcLapTimer.Services;
@@ -5,4 +6,16 @@ namespace TurboKart.Presentation.Service.GrpcLapTimer.Services;
 public class LapTimerService : Greeter.GreeterBase
 {
     
+    private readonly ILogger<GreeterService> _logger;
+
+    public LapTimerService(ILogger<GreeterService> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task<Empty> CartCrossed(CartCrossedRequest request, ServerCallContext context)
+    {
+        var id = request.KartNo;
+        return Task.FromResult(new Empty());
+    }
 }
